2ee2738f39b6def6c3f86a185f75fb0bf9ad0cb8
James.carnell <james.carnell@ibm.com>
2021-10-14 17:03:45 +0100

    black and flake8 pass

--- soil-moisture-sensor/test_app.py
+++ soil-moisture-sensor/test_app.py
@@ -25,9 +25,7 @@ from counterfit_shims_grove.adc import ADC
 
 import sys
 
-sys.path.insert(1, '/soil-moisture-sensor/app.py')
-
-import app
+sys.path.insert(1, "/soil-moisture-sensor/app.py")
 
 # from '../soil-moisture-sensor/app' import adc_read
 
@@ -40,27 +38,27 @@ def test_client_init():
     mock_adc = mock(ADC)
     when(mock_adc).read(0).thenReturn(5)
     assert mock_adc.read(0) == 5
-    
+
 
 def test_process():
     print(app.process(6))
-    assert str(app.process(6)) == "{\"soil_moisture\": 6}"
+    assert str(app.process(6)) == '{"soil_moisture": 6}'
+
 
 # @TODO: write tests
- 
-# UNIT TESTS
-    # adc_read: make sure its only integer - Ash
-    # send: Person 2 - add a return - James & Ash 
-    # process: DONE
-    # handle_method_request: Khadija - might need to be split up?
 
-## File structure - Jacob
+# UNIT TESTS
+# adc_read: make sure its only integer - Ash
+# send: Person 2 - add a return - James & Ash
+# process: DONE
+# handle_method_request: Khadija - might need to be split up?
+
+# File structure - Jacob
 
 # INTEGRATION TESTS
-    # @TODO
+# @TODO
 
 
 # Might be useful for adc_read:
-    # assert type(n) == int, "Incorrect input"
-    #     return n
-
+# assert type(n) == int, "Incorrect input"
+#     return n
