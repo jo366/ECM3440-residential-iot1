3b9cb576b2eb84b4fd240af450e909de00b9cd6a
jacob.osborne <jacobl.osborne1@ibm.com>
2021-11-11 14:48:57 +0000

    integration-test # push

--- soil-moisture-sensor/test_integration_1.py
+++ soil-moisture-sensor/test_integration_1.py
@@ -1,4 +1,6 @@
 import requests
+import app
+import random
 
 
 def counterfit_set():
@@ -16,16 +18,16 @@ def counterfit_set():
             "i2c_unit": "NoUnits",
         },
     )
-
+    print(r)
     # Sleep here for the number of seconds between the app checking plus 2?
     # Now we need to check the IOT hub and we should see a 0 coming out.
     # TODO - How do you tail the IOT hub?
-
+    myNumber = random.randrange(-50, -5)
     r = requests.post(
         "http://127.0.0.1:5000/integer_sensor_settings",
         json={
             "port": "0",
-            "value": 102,  # Make this a random value?
+            "value": myNumber,
             "is_random": False,
             "random_min": 0,
             "random_max": 1023,
@@ -36,6 +38,10 @@ def counterfit_set():
     # Sleep here for the number of seconds between the app checking plus 2?
     # Now we need to check the IOT hub and we should see a whatever the random thing is coming out.
     # TODO - How do you tail the IOT hub?
+
+    soil_moisture = app.adc_read(0)
+
+    assert str(app.process(soil_moisture)) == '{"soil_moisture":' + myNumber + "}"
 
 
 counterfit_set()
